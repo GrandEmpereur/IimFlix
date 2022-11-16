ea526fcaf54c4b255697c7f7fbb885023c01ebc8
[GrandEmpereur] <[bartosikpatrick2@gmail.com]>
2022-11-16 09:35:45 +0100

    :memo: Update

--- src/routes/cdeb.ts
+++ src/routes/cdeb.ts
@@ -1,19 +1,3 @@
-import Router from "koa-router";
-import koaBody from "koa-body";
 
-const ROUTER_OPTIONS = {
-    prefix: '/cdeb'
-};
-
-export default new Router(ROUTER_OPTIONS)
-    .get('/', async (ctx) => {
-        await ctx.render('cdeb', {
-            title: 'CDEB'
-        });
-    })
-
-
-
-// Path: src\routes\index.ts
 
 
--- src/routes/pages.ts
+++ src/routes/pages.ts
@@ -1,19 +1,3 @@
-import Router from "koa-router";
-import koaBody from "koa-body";
 
-const ROUTER_OPTIONS = {
-    prefix: '/'
-};
-
-export default new Router(ROUTER_OPTIONS)
-    .get('/', async (ctx) => {
-        await ctx.render('index', {
-            title: 'Home'
-        });
-    })
-
-
-
-// Path: src\routes\index.ts
 
 
